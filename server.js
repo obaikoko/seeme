7d60a3fb08cb5a522b0dcfff5775fa934bc429f5
obaikoko <jesseobinna7@gmail.com>
2024-05-16 12:49:04 +0100

    updated cors

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
